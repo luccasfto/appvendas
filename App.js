4b14ccd01860c3d223fc51545998f10b08d82013
luccasfto <luccasfto@gmail.com>
2024-03-20 20:23:49 -0300

    desktop version

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { StatusBar, SafeAreaView, View } from 'react-native';
-import Cesta from './src/telas/Cesta';
+
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold} from '@expo-google-fonts/montserrat';
+
+import AppLoading from 'expo-app-loading';
+
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
+
+
 
 export default function App() {
   const [fonteCarregada] = useFonts({
@@ -17,7 +24,7 @@ export default function App() {
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock}/>
     </SafeAreaView>
   );
 }
